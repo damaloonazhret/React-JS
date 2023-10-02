a2f2fca225a15dfac7a06aebd9d490049f13f9fb
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-10-02 19:38:20 +0300

    feat: 11 - layout, grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import './App.scss';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://www.meme-arsenal.com/memes/0c55ae07b3eaf682f7ee9af1594d24a3.jpg" alt="img"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href='google.com'>Profile</a>
+                </div>
+                <div>
+                    <a href='google.com'>Messages</a>
+                </div>
+                <div>
+                    <a href='google.com'>News</a>
+                </div>
+                <div>
+                    <a href='google.com'>Music</a>
+                </div>
+                <div>
+                    <a href='google.com'>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://klike.net/uploads/posts/2022-12/1670484104_3-33.jpg" alt="img"/>
+                </div>
+                <div>
+                    ava + descr
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     )
 }
 
