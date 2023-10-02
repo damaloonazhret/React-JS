a5ff496499e5720c6a7ef87f63306a2830da6588
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-10-02 12:48:43 +0300

    feat: 07 - index.js, App, JSX

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App;
