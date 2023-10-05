5ae86141c53a28f2a0f433ee83dfde06e1f2e92d
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-10-05 14:49:15 +0300

    refactor: 39 - delete return type

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -112,25 +112,13 @@ let store = {
     }
 }
 
-export const addPostActionCreator = () => {
-    return {
-        type: ADD_POST
-    }
-}
+export const addPostActionCreator = () => ({type: ADD_POST})
 
-export const updateNewPostTextActionCreator = (text) => {
-    return {type: UPDATE_NEW_POST_TEXT, newText: text}
-}
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
-export const addMessageActionCreator = () => {
-    return {
-        type: ADD_MESSAGE
-    }
-}
+export const addMessageActionCreator = () => ({type: ADD_MESSAGE})
 
-export const updateNewMessageTextActionCreator = (text) => {
-    return {type: UPDATE_NEW_MESSAGE_TEXT, newText: text}
-}
+export const updateNewMessageTextActionCreator = (text) => ({type: UPDATE_NEW_MESSAGE_TEXT, newText: text})
 
 window.state = store;
 
