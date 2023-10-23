866ce84797a543652b7b810ddfd9439dc79e19cd
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-10-23 22:36:45 +0300

    refactor: 91 - extensions redux react

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import navbarFriendsReducer from "./navbarFriendsReducer";
@@ -18,8 +18,13 @@ const reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
 
-window.store = store;
+// let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+window.__store__ = store;
 
 export default store;
